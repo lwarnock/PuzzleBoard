3a68cb3429cec2e2df564c610ed3c3f20dba91b1
lwarnock <l.warnock@lancaster.ac.uk>
2021-06-26 12:06:24 +0100

    Generated PuzzleBoard and Item components

--- PuzzleBoard-App/src/app/app.module.ts
+++ PuzzleBoard-App/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PuzzleBoardComponent } from './puzzle-board/puzzle-board.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PuzzleBoardComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ PuzzleBoard-App/src/app/puzzle-board/puzzle-board.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-puzzle-board',
+  templateUrl: './puzzle-board.component.html',
+  styleUrls: ['./puzzle-board.component.css']
+})
+
+/**
+ * The PuzzleBoard component is the backboard that holds various items.
+ * 
+ * It contains a limited number of slots and 
+ */
+export class PuzzleBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
